cbd7fb21f2bf89cd6c959d9a9f417bb0457067d2
Eduardo S. Libardi <eslibardi@gmail.com>
2021-10-26 16:32:13 -0300

    initial commit for deployment on netlify

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className='loader'>
+      <h3>loading</h3>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum saepe
+        quae, optio voluptates voluptatum voluptas explicabo, iste, aliquid
+        quidem reiciendis distinctio doloribus odio architecto beatae labore
+        nostrum rerum? Perferendis minus sed porro. A dolores doloremque tempora
+        eligendi blanditiis dolor quisquam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
